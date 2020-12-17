11191da8325cda467012a739c096849df34534a1
Cameron MacFarland <cameron@mobility.com.au>
2020-12-17 12:31:13 +0800

    Created repro

---
+++ TheTest.cs
@@ -0,0 +1,24 @@
+﻿using Bogus;
+using Shouldly;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace ShouldMatchApprovedTestPerf
+{
+    public class TheTest
+    {
+        [Fact]
+        public void LargeApproval()
+        {
+            var faker = new Faker();
+
+            var lines = Enumerable.Range(0, 10000)
+                .Select(_ => faker.Random.String2(10, 100))
+                .ToArray();
+
+            var expected = string.Join(Environment.NewLine, lines);
+            expected.ShouldMatchApproved();
+        }
+    }
+}
